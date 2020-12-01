f67dec0bf0350eaaace94a1bbbdbc838dac372a8
sosoosjsjnab <74925201+sosoosjsjnab@users.noreply.github.com>
2020-12-02 00:53:58 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-mirrorcaptcha","displayName":"MirrorCaptcha","link":{"linkType":"LinkTypePage","href":"#!page-mirrorcaptcha"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
